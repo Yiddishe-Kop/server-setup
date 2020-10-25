ad17f338552d6f78f06a7713d7c993cce143ea5d
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-10-25 22:30:11 +0200

    .env template & replacements

--- 3-project-setuo-and-cd.sh
+++ 3-project-setuo-and-cd.sh
@@ -81,7 +81,7 @@ sudo gpasswd -a "$USER" www-data
 sudo chown -R "$USER":www-data /var/www
 
 cd /var/www/$projectName
-cat .env.example | sed "s/DB_DATABASE=laravel/DB_DATABASE=$projectName/" | sed "s/DB_USERNAME=root/DB_USERNAME=${projectName}_user/" | sed "s/DB_PASSWORD=/DB_PASSWORD=${dbPassword}/" | sed "s/QUEUE_CONNECTION=sync/QUEUE_CONNECTION=database/" > .env
+cat $SCRIPTS_DIR/.env.template | sed "s/%%PROJECT_NAME%%/$projectName/" |  sed "s/%%DB_PASSWORD%%/$dbPassword/" | sed "s/%%DOMAIN_NAME%%/$domainName/" > .env
 php artisan key:generate
 php artisan storage:link
 
